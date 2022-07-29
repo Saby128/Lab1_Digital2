446324377c8e8e9351e4d57f6d8fe04846695eb8
Usuario <Usuario@ASUS-DESKTOP>
2022-07-29 10:29:05 -0600

    Lab1Digital2

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Usuario
+ *
+ * Created on 28 de julio de 2022, 10:23 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
